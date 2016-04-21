1a7fdf19de23f4b6bf36f71b149dc41796bd4a99
J. M. Colmenar <jmcolmenar@gmail.com>
2016-04-21 16:02:49 +0200

    Contribución inicial Chema

---
+++ src/codigo/ClasePrincipal.java
@@ -0,0 +1,14 @@
+package codigo;
+
+/**
+ * Clase principal para ejercicio de pull request.
+ * 
+ * @author J. M. Colmenar
+ */
+public class ClasePrincipal {
+    
+    public static void main(String[] args) {
+        System.out.println("Clase principal, todo por hacer ...");
+    }
+    
+}
