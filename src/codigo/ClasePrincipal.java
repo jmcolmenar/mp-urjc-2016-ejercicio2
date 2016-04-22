cef85868692d40888feefcfb1be2b3e626dec6e5
oly13 <d.oliver13@me.com>
2016-04-22 18:35:00 +0200

    Llamada al metodo porFin en Main()

--- src/codigo/ClasePrincipal.java
+++ src/codigo/ClasePrincipal.java
@@ -9,6 +9,7 @@ public class ClasePrincipal {
     
     public static void main(String[] args) {
         System.out.println("Clase principal, todo por hacer ...");
+        porFin();
     }
     
     public static void porFin(){
