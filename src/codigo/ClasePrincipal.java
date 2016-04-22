cc1f1e140cd60849f285a4fe213b9e92c9e1f5aa
pablogarciabenayas <pablogarciabenayas@gmail.com>
2016-04-22 18:32:47 +0200

    Añadido metodo saludar.

--- src/codigo/ClasePrincipal.java
+++ src/codigo/ClasePrincipal.java
@@ -9,6 +9,12 @@ public class ClasePrincipal {
     
     public static void main(String[] args) {
         System.out.println("Clase principal, todo por hacer ...");
+        saludar("Pablo");
+    }
+    
+    
+    public static void saludar(String nombre){
+        System.out.println("Hola " + nombre + "!");
     }
     
 }
